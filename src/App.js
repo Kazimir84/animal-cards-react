eb29d302efcee7da8cfc77e4e0bcd34147145b73
Kazimir84 <kav2007@ukr.net>
2022-06-09 19:44:19 +0300

    Animal Cards React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import animals from './components/App/data';
+import AnimalCard from './components/AnimalCard/AnimalCard';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            {animals.map(animal =>
+                <AnimalCard
+                    diet={animal.diet}
+                    key={animal.name}
+                    name={animal.name}
+                    size={animal.size}
+                />
+            )}
+        </div>
+    )
 }
 
 export default App;
